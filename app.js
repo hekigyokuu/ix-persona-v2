dc7a76d53840081e0c10a85885bd14e4b1c9eb7b
hekigyokuu <hekiigyokuu@gmail.com>
2025-05-02 22:03:27 +0800

    added route for about and types

--- app.js
+++ app.js
@@ -6,9 +6,21 @@ const PORT = 5000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res)=> {
-    res.sendFile(path.join(__dirname, "index.html"));
-})
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/types", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "html", "types.html"));
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "html", "about.html"));
+});
+
+app.use((req, res) => {
+    res.status(404).send("TUng TUng Tung TUng TUng TUng SAHUR NOT FOUND");
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
