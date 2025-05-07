5043f270c84f109353f609ed892923fef8bb5935
hekigyokuu <hekiigyokuu@gmail.com>
2025-05-07 22:34:08 +0800

    add footer

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get("/about", (req, res) => {
 });
 
 app.use((req, res) => {
-    res.statusCode(404).sendFile(path.join(__dirname, "public", "html", "not-found.html"));
+    res.status(404).sendFile(path.join(__dirname, "public", "html", "not-found.html"));
 });
 
 app.listen(PORT, () => {
