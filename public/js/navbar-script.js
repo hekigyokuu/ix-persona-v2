bb239b677b4d1a1fe8a62e39c9cd0406be159eb7
hekigyokuu <hekiigyokuu@gmail.com>
2025-05-10 17:53:26 +0800

    add some ^%%#!&%%

--- public/js/navbar-script.js
+++ public/js/navbar-script.js
@@ -1,15 +1,44 @@
-const navbar = document.querySelector(".navbar");
+const navbar = document.querySelector('.navbar');
 
-window.addEventListener("scroll", ()=> {
+window.addEventListener('scroll', () => {
     if (window.scrollY == 0) {
-        navbar.classList.remove("scrolled-navbar");
+        navbar.classList.remove('scrolled-navbar');
     } else {
-        navbar.classList.add("scrolled-navbar");
+        navbar.classList.add('scrolled-navbar');
     }
 });
 
-const logo = document.getElementById("web-logo");
+const logo = document.getElementById('web-logo');
 
-logo.addEventListener("click", ()=> {
-    window.location.href = "/";
-})
+logo.addEventListener('click', () => {
+    window.location.href = '/';
+});
+
+// HAHAHHAHAHHAHA
+
+const popup = document.getElementById('popupAd');
+const skipBtn = document.getElementById('skipAdBtn');
+const countdownText = document.getElementById('countdown');
+let timeLeft = 10;
+
+const countdownInterval = setInterval(() => {
+    timeLeft--;
+    countdownText.textContent = `You can skip in ${timeLeft} second${
+        timeLeft === 1 ? '' : 's'
+    }...`;
+
+    if (timeLeft <= 0) {
+        clearInterval(countdownInterval);
+        countdownText.textContent = 'You can now skip the ad.';
+        skipBtn.disabled = false;
+        skipBtn.classList.add('enabled');
+        skipBtn.style.cursor = 'pointer';
+    }
+}, 1000);
+
+skipBtn.addEventListener('click', () => {
+    if (!skipBtn.disabled) {
+        popup.style.display = 'none';
+        document.body.classList.remove('lock-scroll');
+    }
+});
