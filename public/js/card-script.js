b989f829df1d1cb93e611e13db201a6ec35693d2
hekigyokuu <hekiigyokuu@gmail.com>
2025-05-16 11:57:03 +0800

    cards js logic

--- public/js/card-script.js
+++ public/js/card-script.js
@@ -1,3 +1,331 @@
+const types = [
+    {
+        id: 'type-one',
+        title: 'Type 1: The Reformer',
+        cards: [
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+        ],
+    },
+    {
+        id: 'type-two',
+        title: 'Type 2: The Helper',
+        cards: [
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+        ],
+    },
+    {
+        id: 'type-three',
+        title: 'Type 3: The Achiever',
+        cards: [
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+        ],
+    },
+    {
+        id: 'type-four',
+        title: 'Type 4: The Individualist',
+        cards: [
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+        ],
+    },
+    {
+        id: 'type-five',
+        title: 'Type 5: The Investigator',
+        cards: [
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+        ],
+    },
+    {
+        id: 'type-six',
+        title: 'Type 6: The Loyalist',
+        cards: [
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+        ],
+    },
+    {
+        id: 'type-seven',
+        title: 'Type 7: The Enthusiast',
+        cards: [
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+        ],
+    },
+    {
+        id: 'type-eight',
+        title: 'Type 8: The Challenger',
+        cards: [
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+        ],
+    },
+    {
+        id: 'type-nine',
+        title: 'Type 9: The Peacemaker',
+        cards: [
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+            {
+                type: 'SP',
+                name: 'Name of Card',
+                briefDesc:
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Flip card for full details.',
+                fullDesc:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique saepe quos reprehenderit dicta nemo magni fugit, ducimus voluptatum illum fugiat tempore consequuntur adipisci, maiores recusandae molestias officia incidunt eaque.',
+            },
+        ],
+    },
+];
+
+function createCard(cardData) {
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    card.innerHTML = `
+    <div class="card-inner">
+      <div class="card-front">
+        <div class="card-name">
+          <div class="card-type">${cardData.type}</div>
+          ${cardData.name}
+        </div>
+        <div class="card-img"></div>
+        <div class="card-brief-description">${cardData.briefDesc}</div>
+      </div>
+      <div class="card-back">
+        <div class="card-full-description">${cardData.fullDesc}</div>
+      </div>
+    </div>
+  `;
+    return card;
+}
+
+function createType(typeData) {
+    const typeDiv = document.createElement('div');
+    typeDiv.id = typeData.id;
+    typeDiv.className = 'type';
+
+    const heading = document.createElement('h3');
+    heading.textContent = typeData.title;
+    typeDiv.appendChild(heading);
+
+    const cardContainer = document.createElement('div');
+    cardContainer.className = 'card-container';
+
+    typeData.cards.forEach((cardData) => {
+        const card = createCard(cardData);
+        cardContainer.appendChild(card);
+    });
+
+    typeDiv.appendChild(cardContainer);
+
+    const typeBottom = document.createElement('div');
+    typeBottom.className = 'type-bottom';
+    typeDiv.appendChild(typeBottom);
+
+    return typeDiv;
+}
+
+const container = document.getElementById('types-container-personalities');
+
+types.forEach((typeData) => {
+    const typeElement = createType(typeData);
+    container.appendChild(typeElement);
+});
+
 document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
         card.classList.toggle('flipped');
